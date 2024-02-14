886a6c7348ff08f33a5be24596dfe55d57176434
Melker Carlsson <melker.carlsson@hotmail.se>
2024-02-14 20:08:46 +0100

    hejjjj

--- native_regression.py
+++ native_regression.py
@@ -4,3 +4,12 @@ inp_fileTwo= open(sys.argv[1], encoding="utf-8")
 T=str(inp_fileTwo)
 Matrix=m.loadtxt(T)
 inp_fileTwo.close()
+X=0
+Y=0
+
+#innan detta måste vi definera x och y utifrån matrisen vi får från loadtxt vilket står vad somm är x och y i instruktionerna 
+Xp  = m.powers(X,0,1)
+Yp  = m.powers(Y,1,1)
+Xpt = m.transpose(Xp)
+
+[[b],[m]] = m.matmul(m.invert(m.matmul(Xpt,Xp)),m.matmul(Xpt,Yp))
